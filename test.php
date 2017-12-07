156903eecc8870402f3fd5dba26e0cab840de486
Ragnorhub <32896505+Ragnorhub@users.noreply.github.com>
2017-12-07 18:06:08 +0300

    1

--- test.php
+++ test.php
@@ -1,4 +1,20 @@
-<?php
-  $p = sqrt(25);
-  echo $p;
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+      $p = sqrt(25);
+      echo 2 + 51 ;
+      echo "Привет мир!";
+      var_dump($p);
+      $d = abcdef;
+      echo $e = $d[2];
+      var_dump($e);
+      echo 3-true;
+      echo 3-false;
+    ?>
+  </body>
+</html>
